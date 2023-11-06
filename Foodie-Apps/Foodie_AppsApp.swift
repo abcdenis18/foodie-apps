ee73f81b394be27984b46960626bde1bc0175091
abcdenis18 <denisfajar24@gmail.com>
2023-11-06 21:18:05 +0700

    Initial Commit

---
+++ Foodie-Apps/Foodie_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foodie_AppsApp.swift
+//  Foodie-Apps
+//
+//  Created by Denis Fajar Sidik on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Foodie_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
